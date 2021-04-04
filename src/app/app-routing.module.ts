9f5a748a548dfeaa056704a5021f1799d1332cb4
Akshaya <akshayarh@gmail.com>
2021-04-04 17:14:59 +0530

    first commit

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { CreateAccountComponent } from './create-account/create-account.component'
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path:'welcome',
+    component:WelcomeComponent
+  },
+  {
+    path:'createAccount',
+    component:CreateAccountComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,19 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
-
+import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { LoginComponent } from './login/login.component';
+import { WelcomeComponent } from './welcome/welcome.component';
+import { CreateAccountComponent } from './create-account/create-account.component';
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    HeaderComponent,
+    FooterComponent,
+    LoginComponent,
+    WelcomeComponent,
+    CreateAccountComponent,
   ],
   imports: [
     BrowserModule,
